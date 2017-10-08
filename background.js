8ceadca4aca0d107f8796f3745a9628a2ed2aac0
Cody Chan <int64ago@gmail.com>
2017-10-08 20:14:52 +0800

    feat: enable auto reload

--- background.js
+++ background.js
@@ -7,9 +7,10 @@ var flashAndReload = function(noReload) {
   }
   bm.clearHostResolverCache();
   bm.closeConnections();
-  if (!noReload) {
+  if (!noReload && tabs) {
     tabs.reload();
   }
+  console.log('[flashAndReload]');
 };
 
 chrome.browserAction.onClicked.addListener(function(){
@@ -28,4 +29,37 @@ chrome.commands.onCommand.addListener(function(cmd) {
   if (cmd === 'flash-and-reload') {
     flashAndReload();
   }
+});
+
+var options = {
+  enable: localStorage.getItem('auto.refresh.enable') === 'true',
+  interval: (localStorage.getItem('auto.refresh.interval') || 0) / 1,
+};
+
+var autoReload = null;
+
+function clearAutoReload() {
+    clearInterval(autoReload);
+    autoReload = null;
+    console.log('[AutoReload] stop');
+}
+
+function setAutoReload(options) {
+  if (options.enable && options.interval) {
+    if (autoReload) {
+      clearAutoReload();
+    }
+    autoReload = setInterval(() => {
+      flashAndReload(true);
+    }, options.interval * 1000);
+    console.log('[AutoReload] start');
+  } else if (autoReload) {
+    clearAutoReload();
+  }
+}
+
+setAutoReload(options);
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    setAutoReload(message);
 });
--- options.js
+++ options.js
@@ -1,14 +1,38 @@
 (function () {
 
+  if (!chrome.benchmarking) {
+    location.href = 'https://goo.gl/vSh9im';
+  }
+
   const enable = document.getElementById('enable');
   const interval = document.getElementById('interval');
 
+  const options = {
+    enable: localStorage.getItem('auto.refresh.enable') === 'true',
+    interval: (localStorage.getItem('auto.refresh.interval') || 0) / 1,
+  };
+
+  enable.checked = options.enable;
+  interval.value = options.interval;
+
   interval.addEventListener('input', e => {
     const value = e.target.value;
+    options.interval = value;
+    localStorage.setItem('auto.refresh.interval', value);
+    setting();
   });
 
   enable.addEventListener('change', e => {
     const value = e.target.checked;
+    options.enable = value;
+    localStorage.setItem('auto.refresh.enable', value);
+    setting();
   });
+
+  function setting() {
+    chrome.runtime.sendMessage(options, resp => {
+      console.log(options);
+    });
+  }
 
 })();
